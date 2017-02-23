ba245b386a7adcf73f0e8aa6225edef57409156f
Mathias Maisberger <hiasinho@me.com>
2017-02-23 08:21:20 +0100

    Add events

---
+++ src/_webpack/javascripts/lib/Analytics.js
@@ -0,0 +1,48 @@
+function isElementInView(element, fullyInView) {
+  var pageTop = $(window).scrollTop()
+  var pageBottom = pageTop + $(window).height()
+  var elementTop = $(element).offset().top
+  var elementBottom = elementTop + $(element).height()
+
+  if (fullyInView === true) {
+    return ((pageTop < elementTop) && (pageBottom > elementBottom))
+  } else {
+    return ((elementTop <= pageBottom) && (elementBottom >= pageTop))
+  }
+}
+
+export default class Analytics {
+  static track(category, action, label = null, value = null) {
+    const eventData = {
+      hitType: 'event',
+      eventCategory: category,
+      eventAction: action
+    }
+
+    if (label != null) eventData['eventLabel'] = label
+    if (value != null) eventData['eventValue'] = value
+
+    console.log(eventData)
+    ga('send', eventData)
+  }
+
+  static trackSiteEvents() {
+    $('.js-track-visible').one('gettingVisible', function(ev) {
+      const data = $(this).data()
+      Analytics.track(data.trackCategory, data.trackAction, data.trackLabel, data.trackValue)
+    })
+
+    $('.js-track-click').on('click', function(ev) {
+      Analytics.track('Link', 'click', this.href)
+    })
+
+    $(window).scroll(function() {
+      const elements = $('.js-track-visible')
+      elements.each(function() {
+        if (isElementInView(this)) {
+          $(this).trigger('gettingVisible')
+        }
+      })
+    })
+  }
+}
