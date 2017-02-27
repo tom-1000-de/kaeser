6771b7c6517689aeb37dabdceb170538a6e5f0f2
Mathias Maisberger <hiasinho@me.com>
2017-02-27 05:43:59 +0100

    Fix deploy script

--- bin/deploy_circleci.sh
+++ bin/deploy_circleci.sh
@@ -3,21 +3,20 @@
 set -e
 
 # show where we are on the machine
-pwd
-remote=$(git config remote.origin.url)
+# pwd
+# remote=$(git config remote.origin.url)
 
 # now lets setup a new repo so we can update the gh-pages branch
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
-git init
-git remote add --fetch origin "$remote"
+# git remote add --fetch origin "$remote"
 
 echo check out branch gh-pages ...
 git checkout -b gh-pages
 echo add dist folder
 git add -f dist
 echo commit changes
-git commit -m ":ship: :date: $(date)"
+git commit -m ":ship: :date: $(date) [ci skip]"
 echo push to remote gh-pages
 git push origin `git subtree split --prefix dist`:gh-pages --force
 echo checkout branch master
