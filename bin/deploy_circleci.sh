8eecd9cb70fd6e3ef6b5bfc8664531a0ed7614a9
Mathias Maisberger <hiasinho@me.com>
2017-02-27 06:02:06 +0100

    Fix typos

--- bin/deploy_circleci.sh
+++ bin/deploy_circleci.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
-# abort the script if there is a non-zero error
 set -e
-
-# show where we are on the machine
-# pwd
-# remote=$(git config remote.origin.url)
 
 # now lets setup a new repo so we can update the gh-pages branch
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
-# git remote add --fetch origin "$remote"
 
 echo check out branch gh-pages ...
 git checkout -b gh-pages
